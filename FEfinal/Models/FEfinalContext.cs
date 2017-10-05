0a2af5ae84c0f5abd3d75aaeb9c9a2935f65b9ed
venkats2000 <sgv2008@gmail.com>
2017-10-04 23:38:37 -0600

    Add project files.

---
+++ FEfinal/Models/FEfinalContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FEfinal.Models
+{
+    public class FEfinalContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public FEfinalContext() : base("name=FEfinalContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<FEfinal.Models.FileEnc> FileEncs { get; set; }
+    }
+}
---
+++ FEfinal/Models/FileEnc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FEfinal.Models
+{
+    public class FileEnc
+    {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "File URL")]
+        public String FileLocation { get; set; }
+        [Display(Name = "Expiry date")]
+        [DataType(DataType.Date)]
+        public DateTime ExpiryDate { get; set; }
+        public string FileType { get; set; }
+        public string Enc_String { get; set; }
+    }
+}
+
+ 
